79a1f68213a8b5cf99983f0e804cff05ab7e20e9
ext.caoyaowei1 <ext.caoyaowei1@jd.com>
2021-09-28 18:42:37 +0800

    nadata

--- src/components/noData/index.md
+++ src/components/noData/index.md
@@ -9,11 +9,30 @@ nav:
 
 Demo:
 
+```tsx
+import React from 'react';
+import { NoData, Header } from 'library';
+
+export default () => (
+  <div>
+    <Header>你好，我是测试标题</Header>
+    <NoData top={200}></NoData>
+    <NoData label="暂无库存信息" top={200}></NoData>
+  </div>
+);
+```
+
 ```tsx | phone
 import React from 'react';
-import { NoData } from 'library';
+import { NoData, Header } from 'library';
 
-export default () => <NoData className="jdd-noData"></NoData>;
+export default () => (
+  <div>
+    <Header goBack={() => {}} title="我是一个很努力的小header" />
+    <NoData top={200}></NoData>
+    <NoData label="我是一个很空很空的容器，我只存放图片和我" top={200}></NoData>
+  </div>
+);
 ```
 
 <API src="./index.tsx"></API>
--- src/components/noData/index.tsx
+++ src/components/noData/index.tsx
@@ -1,7 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-27 18:35:54
+ * @LastEditTime: 2021-09-28 16:49:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \library\src\components\noData\index.tsx
+ */
 import React from 'react';
-import { NoDataProps } from './interface';
+import { Props } from './interface';
 import './index.less';
+import notask from './noData.svg';
 
-const NoData: React.FC<NoDataProps> = () => <h1>Hello,world</h1>;
-NoData.displayName = 'NoData';
+const NoData: React.FC<Props> = (userProps) => {
+  const {
+    label = '暂无数据',
+    imgUrl = notask,
+    imgWidth = 160,
+    imgHeight = 160,
+    className = '',
+  } = userProps;
+
+  return (
+    <div className="jdd-noData">
+      <img width={imgWidth + 'px'} height={imgHeight + 'px'} src={imgUrl} />
+      <div className="label">{label || '暂无数据'}</div>
+    </div>
+  );
+};
+
 export default NoData;
