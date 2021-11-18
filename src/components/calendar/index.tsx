fa29ff0a3eb124e53debf3780c0bdf0f983fe563
ext.yaojiaqi1 <ext.yaojiaqi1@jd.com>
2021-11-18 14:37:46 +0800

    添加prop-types依赖

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -2,8 +2,8 @@
  * @Author: 李闪闪
  * @Date: 2021-09-24 09:53:38
  * @Email: lishanshan6@jd.com
- * @LastEditors: 李闪闪
- * @LastEditTime: 2021-10-20 10:54:18
+ * @LastEditors: 姚嘉琦
+ * @LastEditTime: 2021-11-18 14:26:37
  * @Description:
  */
 import * as PropTypes from 'prop-types';
@@ -12,7 +12,7 @@ import { Calendar as RMCalendar } from 'rmc-calendar';
 import { getComponentLocale } from '@/utils/getLocale';
 import { CalendarProps } from './interface';
 import Icon from '../icon';
-import '@/components/icon/index.less';
+
 import './index.less';
 
 class Calendar extends React.Component<CalendarProps, any> {
